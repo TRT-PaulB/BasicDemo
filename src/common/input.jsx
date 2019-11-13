6b9d32f653ed4ebd53d77e324294603c41322b4a
TRT-PaulB <paul.bickell@raytheon.co.uk>
2019-11-13 16:22:40 +0000

    notes in input

--- src/common/input.jsx
+++ src/common/input.jsx
@@ -17,3 +17,55 @@ const Input = ({ name, label, error, width, ...rest }) => {
 
 export default Input;
 
+// https://github.com/TRT-PaulB/RoutePlannerBase2/blob/master/react-routeplanner-app/src/components/login.jsx
+// https://github.com/TRT-PaulB/RoutePlannerBase2/blob/master/react-routeplanner-app/src/components/logout.jsx
+// https://github.com/TRT-PaulB/RoutePlannerBase2/blob/master/react-routeplanner-app/src/services/authService.js
+// https://github.com/TRT-PaulB/RoutePlannerBase2/blob/master/react-routeplanner-app/src/services/httpService.js
+// https://github.com/TRT-PaulB/RoutePlannerBase2/blob/master/react-routeplanner-app/src/common/navbar.jsx
+
+////////////////////////////////////////////////
+// LOGIN doSubmit
+
+// await auth.login(username, password);
+
+//       // see the redirected state could be set in protectedRoute.jsx
+//       const { state } = this.props.location;
+//       window.location = state ? state.from.pathname : "/";
+
+////////////////////////////////////////////////
+// HTTP SERVICE
+
+// export function setJwt(jwt) {
+//   axios.defaults.headers.common["x-auth-token"] = jwt;
+// }
+
+////////////////////////////////////////////////
+//AUTH SERVICE
+
+// export async function login(email, password) {
+//   const { data: jwt } = await http.post(authEndpoint, {
+//     email: email,
+//     password: password
+//   });
+
+//   console.log(jwt);
+//   localStorage.setItem(tokenKey, jwt);
+// }
+
+// export function logout() {
+//   localStorage.removeItem(tokenKey);
+// }
+
+// export function getJwt() {
+//   return localStorage.getItem(tokenKey);
+// }
+
+// export default {
+//   login,
+//   logout,
+//   getCurentUser,
+//   loginWithJwt,
+//   getJwt
+// };
+
+////////////////////////////////////////////////
